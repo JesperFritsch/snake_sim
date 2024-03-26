f911d36a36271c05c091b20a2084f4a2beb77248
Jesper Fritsch <jesperf96@gmail.com>
2024-03-26 21:43:55 +0100

    Changes

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     snake_init_len = 5
     env = SnakeEnv(GRID_WIDTH, GRID_HEIGHT, FOOD)
     env.add_snake(AutoSnake('A', snake_init_len), (176, 27, 16), (125, 19, 11))
-    # env.add_snake(AutoSnake('B', snake_init_len), (19, 44, 209), (8, 23, 120))
-    # env.add_snake(AutoSnake('C', snake_init_len), (19, 212, 77), (10, 140, 49))
-    # env.add_snake(AutoSnake('D', snake_init_len), (128, 3, 111), (199, 4, 173))
+    env.add_snake(AutoSnake('B', snake_init_len), (19, 44, 209), (8, 23, 120))
+    env.add_snake(AutoSnake('C', snake_init_len), (19, 212, 77), (10, 140, 49))
+    env.add_snake(AutoSnake('D', snake_init_len), (128, 3, 111), (199, 4, 173))
 
-    for _ in range(1):
+    for _ in range(100):
         env.generate_run()
         env.reset()
 
