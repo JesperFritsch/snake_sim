a0da8dccf9d03ec734290b65e7a92690041d7748
Jesper Fritsch <jesperf96@gmail.com>
2024-03-30 11:45:08 +0100

    Update

--- snakes/autoSnake3.py
+++ snakes/autoSnake3.py
@@ -136,7 +136,7 @@ class AutoSnake3(AutoSnakeBase):
 
     def is_area_clear(self, s_map, body_coords, start_coord, checked=None):
         current_coords = [start_coord]
-        safety_buffer = 5
+        safety_buffer = 0
         if checked is None:
             checked = np.array([False] * (self.env.height * self.env.width), dtype=bool)
         body_len = len(body_coords)
@@ -192,6 +192,7 @@ class AutoSnake3(AutoSnakeBase):
             current_results['apple_time'] = current_results['apple_time'] + [depth]
             current_results['len_gain'] = length - self.length
         current_results['depth'] = depth
+        current_results['body_coords'] = body_coords
         old_tail = self.update_body(new_coord, body_coords, length)
         s_map = self.update_snake_position(s_map, body_coords, old_tail)
         # if not route:
@@ -201,6 +202,7 @@ class AutoSnake3(AutoSnakeBase):
         best_results['depth'] = max(best_results.get('depth', 0), current_results['depth'])
         best_results['len_gain'] = max(best_results.get('len_gain', 0), current_results['len_gain'])
         best_results['apple_time'] = min(best_results, current_results, key=lambda x: sum(x.get('apple_time', [length*length])[:5]))['apple_time']
+        best_results['body_coords'] = None
         valid_tiles.sort(key=lambda x: 0 if x == target_tile else 1)
         if ((time() - start_time) * 1000 > self.MAX_BRANCH_TIME) and self.TIME_LIMIT:
             best_results['timeout'] = True
