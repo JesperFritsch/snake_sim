b3e71c60e34a4a513c1ff379a138f10d3df59eab
Jesper Fritsch <jesperf96@gmail.com>
2024-05-05 23:42:59 +0200

    improving the video rendering

--- render/video_render.py
+++ render/video_render.py
@@ -11,14 +11,33 @@ import cv2
 import numpy as np
 
 
-def make_video(run_file, out_dir, fps, frames_multiply=1):
-    run_file_path = Path(run_file)
-    size = (640, 640)
-    filebasename = run_file_path.stem + '.mp4'
+def frames_to_video(frames, output_abs_path, fps, size=None):
+    """
+    Converts a list of frames to a video file using opencv.
+    The file ending has to be .mp4
+    frames: list of frames, each frame is a numpy array with shape (height, width, 3) 3 being the RGB channels
+    """
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    output_path = Path(out_dir).joinpath(filebasename)
-    out = cv2.VideoWriter(str(output_path), fourcc, fps, size)
+    out = cv2.VideoWriter(str(output_abs_path), fourcc, fps, size)
 
+    for frame in frames:
+        bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        if size is not None:
+            final_frame = cv2.resize(bgr_frame, size, interpolation=cv2.INTER_AREA)
+        else:
+            final_frame = bgr_frame
+        out.write(final_frame)
+
+    # Release everything when job is finished
+    out.release()
+
+
+def make_video(run_file, out_dir, fps, frames_multiply=1):
+    print('making video for', run_file.stem)
+    run_file_path = Path(run_file)
+    filebasename = run_file_path.stem + '.mp4'
+    output_path = Path(out_dir).joinpath(filebasename)
+    frames = []
     with open(Path(run_file)) as run_file:
         run_data = json.load(run_file)
         metadata = run_data.copy()
@@ -27,28 +46,31 @@ def make_video(run_file, out_dir, fps, frames_multiply=1):
         for step_nr, step_data in run_data['steps'].items():
             subframes = frame_builder.step_to_frames(step_data)
             for frame in subframes:
-                bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                resized_frame_image = cv2.resize(bgr_frame, size, interpolation=cv2.INTER_AREA)
                 for _ in range(frames_multiply):
-                    out.write(resized_frame_image)
-
-    # Release everything when job is finished
-    out.release()
+                    frames.append(frame)
+    frames_to_video(frames, output_path, fps, size=(640, 640))
 
 def cli(argv):
     ap = argparse.ArgumentParser()
-    ap.add_argument('-f', '--file', type=str, required=True)
+    ap.add_argument('-f', '--file', type=str)
     args = ap.parse_args(argv)
     if not args.file:
         raise ValueError("No file specified")
     return args
 
 def main(argv):
-    runs = Path('b:/pythonStuff/snake_sim/runs/grid_32x32').resolve()
-    output_dir = Path('render/videos')
-    for run_file in runs.glob('*.json'):
-        print('making video for', run_file.stem)
+    args = cli(argv)
+    script_location = Path(__file__).resolve().parent
+    runs = script_location.joinpath('../runs/grid_32x32').resolve()
+    output_dir = script_location.joinpath('videos').resolve()
+    if args.file:
+        run_file = Path(args.file)
         make_video(run_file, output_dir, fps=30, frames_multiply=2)
+        return
+    else:
+        for run_file in runs.glob('*.json'):
+            make_video(run_file, output_dir, fps=30, frames_multiply=2)
+            break
 
 
 if __name__ == '__main__':
