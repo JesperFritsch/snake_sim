b0405e40a4ae2c6e972b362604741455295a2969
Jesper Fritsch <jesper.fritsch@infotiv.se>
2025-09-18 00:58:07 +0200

    finalizing shared memory snakes

--- snake_sim/environment/snake_updaters/inproc_updater.py
+++ snake_sim/environment/snake_updaters/inproc_updater.py
@@ -1,15 +1,16 @@
 
 from typing import Tuple, List, Dict
 
-from snake_sim.environment.interfaces.ISnakeUpdater import ISnakeUpdater
+from snake_sim.environment.interfaces.snake_updater_interface import ISnakeUpdater
 from snake_sim.environment.interfaces.snake_interface import ISnake
 from snake_sim.environment.types import Coord, EnvData
 
 class InprocUpdater(ISnakeUpdater):
     def __init__(self):
+        super().__init__()
         pass
 
-    def get_decisions(self, snakes: List[ISnake], env_data: EnvData, timeout: int) -> Dict[int, Coord]:
+    def get_decisions(self, snakes: List[ISnake], env_data: EnvData, timeout: float) -> Dict[int, Coord]:
         decisions = {}
         for snake in snakes:
             decisions[snake.get_id()] = snake.update(env_data)
