5f5e87f67319dc4f1950d33bc3b82031bdd85ce4
JesperFritsch <90257090+JesperFritsch@users.noreply.github.com>
2025-09-17 09:45:42 +0200

    Update shm_updater.py

--- snake_sim/environment/snake_updaters/shm_updater.py
+++ snake_sim/environment/snake_updaters/shm_updater.py
@@ -1,6 +1,7 @@
 
 from typing import Tuple, List, Dict
 
+from uuid import uuid4
 from snake_sim.environment.snake_updaters.concurrent_updater import ConcurrentUpdater
 from snake_sim.snakes.shm_proxy_snake import SHMProxySnake
 from snake_sim.environment.types import Coord, EnvData
@@ -11,21 +12,13 @@ class SHMUpdater(ConcurrentUpdater):
     def __init__(self, ):
         super().__init__()
         self._shm_writer: SharedMemoryWriter = None
-        self._snake_reader_map: Dict[int, int] = {}  # snake id to reader id
-        self._next_reader_id = 0
+        self._added_snakes = set()
 
     def register_snake(self, snake: SHMProxySnake):
-        if self._shm_writer is None:
-            raise RuntimeError("SHMUpdater not initialized with SharedMemoryWriter.")
-        snake_id = snake.get_id()
-        if snake_id is None:
-            raise ValueError("Snake ID is not set. Cannot register snake.")
-        if snake_id in self._snake_reader_map:
+        if snake_id in self._added_snakes:
             raise ValueError(f"Snake with ID {snake_id} is already registered.")
-        reader_id = self._next_reader_id
-        self._next_reader_id += 1
-        self._snake_reader_map[snake_id] = reader_id
-        snake.set_reader_id(reader_id)
+        snake.set_reader_id(self._snake_count)
+        self._added_snakes.insert(snake)
         super().register_snake(snake)
 
     def get_decisions(self, snakes: List[SHMProxySnake], env_data: EnvData) -> dict[int, Coord]:
@@ -39,5 +32,10 @@ class SHMUpdater(ConcurrentUpdater):
         super().close()
 
     def finalize(self):
+        shm_name = str(uuid4())
+        shm_reader_count = self._snake_count
+        payload_size = #Size of map
+        raise RuntimeError("SHM updater not complete")
+        self._shm_writer = SharedMemoryWriter.create()
         return super().finalize()
     
