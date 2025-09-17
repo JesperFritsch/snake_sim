9ac52276f44199d4def49c1001cdf24afe49bab0
JesperFritsch <90257090+JesperFritsch@users.noreply.github.com>
2025-09-17 09:31:11 +0200

    Rename ISnakeUpdater.py to snake_updater_interface.py

--- snake_sim/environment/interfaces/snake_updater_interface.py
+++ snake_sim/environment/interfaces/snake_updater_interface.py
@@ -1,28 +1,21 @@
-import logging
-from pathlib import Path
 from abc import ABC, abstractmethod
 from typing import Tuple, List, Dict
 
-from snake_sim.environment.types import Coord, EnvData, EnvInitData
+from snake_sim.environment.types import Coord, EnvData
 from snake_sim.environment.interfaces.snake_interface import ISnake
-
-log = logging.getLogger(Path(__file__).stem)
 
 class ISnakeUpdater(ABC):
 
-    def __init__(self):
-        self._snake_count = 0
-    
     @abstractmethod
-    def get_decisions(self, snakes: List[ISnake], env_data: EnvData, timeout: float) -> Dict[int, Coord]: # -> dict of snake id to direction
+    def get_decisions(self, snakes: List[ISnake], env_data: EnvData, timeout: int) -> Dict[int, Coord]: # -> dict of snake id to direction
         pass
 
     def close(self):
         pass
 
     def register_snake(self, snake: ISnake):
-        self._snake_count += 1
+        pass
 
-    def finalize(self, env_init_data: EnvInitData):
-        log.debug(f"Finalizing updater {self.__class__.__name__} with {self._snake_count} snakes")
+    def finalize(self):
+        pass
 
