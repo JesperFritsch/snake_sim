a4b5d8dd0a8bfb7aa109a06d2b9f78e3e19a6cb7
Jesper Fritsch <jesper.fritsch@infotiv.se>
2026-04-04 00:54:32 +0200

    change the loop setup

--- snake_sim/environment/snake_loop_control.py
+++ snake_sim/environment/snake_loop_control.py
@@ -73,10 +73,18 @@ class SnakeLoopControl:
             # Initialize game loop add keyboard controllers
             self._loop = GameLoop()
             self._loop.set_steps_per_sec(self._config.steps_per_sec)
-
+            self._initialize_player_snakes()
         else:
             # Initialize simulation loop
             self._loop = SimLoop()
+
+        if self._config.distributed_snakes:
+            self._initialize_distributed_snakes()
+        else:
+            self._initialize_inproc_snakes()
+
+        self._initialize_remote_grpcs()
+        self._finalize_snakes()
         self._loop.set_snake_handler(self._snake_handler)
         self._loop.set_environment(self._snake_environment)
         if default_config.max_steps is not None:
@@ -178,18 +186,18 @@ class SnakeLoopControl:
                     log.debug("Stop flag set, stopping loop")
                     self._loop.stop()
                 threading.Thread(target=wait_stop_flag, args=(stop_flag,), daemon=True).start()
-            try:
-                if self._config.distributed_snakes:
-                    self._initialize_distributed_snakes()
-                else:
-                    self._initialize_inproc_snakes()
-                self._initialize_player_snakes()
-                self._initialize_remote_grpcs()
-                self._finalize_snakes()
-            except:
-                if stop_flag is not None:
-                    stop_flag.value = True
-                raise
+            # try:
+            #     if self._config.distributed_snakes:
+            #         self._initialize_distributed_snakes()
+            #     else:
+            #         self._initialize_inproc_snakes()
+            #     self._initialize_player_snakes()
+            #     self._initialize_remote_grpcs()
+            #     self._finalize_snakes()
+            # except:
+            #     if stop_flag is not None:
+            #         stop_flag.value = True
+            #     raise
             self._loop.start()
         except KeyboardInterrupt:
             log.debug("KeyboardInterrupt received, shutting down loop")
