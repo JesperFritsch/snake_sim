2e4464a84746db9f92ea86c6b4fd03359d469785
Jesper Fritsch <jesperf96@gmail.com>
2026-05-15 19:07:47 +0200

    Remove unecessary thread join

--- snake_sim/loop_observables/socket_observable.py
+++ snake_sim/loop_observables/socket_observable.py
@@ -173,8 +173,6 @@ class SocketObservable(ILoopObservable):
             self._listener.close()
         except OSError:
             pass
-        if self._thread and self._thread.is_alive():
-            self._thread.join(timeout=5)
 
     def close(self):
         self.stop()
