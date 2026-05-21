dde65d025136f5a6b895d39b099e6098d0390922
Jesper Fritsch <jesperf96@gmail.com>
2026-05-22 00:03:17 +0200

    Rebuild snake factory and init procedure, add arguments

--- snake_sim/environment/snake_factory.py
+++ snake_sim/environment/snake_factory.py
@@ -24,6 +24,7 @@ except ImportError:
 
 TYPENAME_TO_CLASS = {
     'survivor': SurvivorSnake,
+    'remote': GRPCProxySnake
 }
 
 if PPOSnake:
@@ -39,12 +40,6 @@ class SnakeFactory(metaclass=SingletonMeta):
     def __init__(self):
         pass
 
-    def _create_proxy_snake(self, target: str, proc_type: SnakeProcType) -> ISnake:
-        if proc_type == SnakeProcType.GRPC:
-            return GRPCProxySnake(target)
-        elif proc_type == SnakeProcType.SHM:
-            return SHMProxySnake(target)
-
     def _create_snake_from_config(self, snake_config: SnakeConfig) -> ISnake:
         try:
             snake_class = TYPENAME_TO_CLASS[snake_config.type]
@@ -57,22 +52,9 @@ class SnakeFactory(metaclass=SingletonMeta):
 
     def create_snake(
             self,
-            proc_type: SnakeProcType=None,
-            snake_config: SnakeConfig=None,
-            target: str=None
+            snake_config: SnakeConfig,
         ) -> ISnake:
-        """
-        Creates a snake of the given type. If snake_config is provided,
-        creates a local snake based on the config.
-        If proc_type and target are provided, creates a proxy snake that connects to the given target.
-        """
-        if snake_config:
-            return self._create_snake_from_config(snake_config)
-        elif proc_type and target:
-            proxy_snake = self._create_proxy_snake(target, proc_type)
-            return proxy_snake
-        else:
-            raise ValueError("Either snake_config or both proc_type and target must be provided")
+        return self._create_snake_from_config(snake_config)
 
     def create_many_snakes(
             self,
